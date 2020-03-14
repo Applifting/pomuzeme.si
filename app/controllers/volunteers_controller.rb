1736fdd15aa2855db5b3b3d668b5f5569c8327f3
Kamil Hanus <kamil.hanus@applifting.cz>
2020-03-14 16:13:42 +0100

    agreements check & sms error handler

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,8 +1,7 @@
 class VolunteersController < ApplicationController
   def register
     volunteer = Volunteer.new(volunteer_params).with_existing_record
-    if volunteer.valid?
-      save_and_send_code volunteer
+    if volunteer.valid? && agreements_granted?(volunteer) && save_and_send_code(volunteer)
       render 'volunteer/register_success'
     else
       render 'volunteer/register_error', locals: { volunteer: volunteer }
@@ -29,9 +28,26 @@ class VolunteersController < ApplicationController
     params.permit :confirmation_code
   end
 
+  def agreements_params
+    params.require(:volunteer).permit(:terms_of_service, :age_confirmed)
+  end
+
   def save_and_send_code(volunteer)
-    volunteer.save!
-    volunteer.obtain_confirmation_code
-    session[:volunteer] = volunteer.id
+    ActiveRecord::Base.transaction do
+      volunteer.save!
+      volunteer.obtain_confirmation_code
+      session[:volunteer] = volunteer.id
+      true
+    rescue StandardError
+      volunteer.errors.add(:base, :sms_not_working)
+      raise ActiveRecord::Rollback
+    end
+  end
+
+  def agreements_granted?(volunteer)
+    volunteer.errors.add(:base, :terms_of_service_required) if agreements_params[:terms_of_service] != '1'
+    volunteer.errors.add(:base, :age_confirmed_required) if agreements_params[:age_confirmed] != '1'
+
+    volunteer.errors.empty?
   end
 end
