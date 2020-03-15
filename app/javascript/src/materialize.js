8af32647c2b7d27eccb0ff1a260c682fb09d42ae
Pavel Cerny <pavel.cerny@applifting.cz>
2020-03-15 07:33:55 +0100

    Add tooltip, add styles for registration and errors

--- app/javascript/src/materialize.js
+++ app/javascript/src/materialize.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems, {});
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems, {});
+});
