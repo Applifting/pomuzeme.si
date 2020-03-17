9d6583ba9c7ea2a577be6b2d26e56860631f4384
martintomas <tomas@jchsoft.cz>
2020-03-17 20:33:05 +0100

    Added phone validation to Request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Request < ApplicationRecord
+  phony_normalize :subscriber_phone, default_country_code: 'CZ'
+  phony_normalized_method :subscriber_phone, default_country_code: 'CZ'
+
   belongs_to :created_by, class_name: 'User'
   belongs_to :closed_by, class_name: 'User', optional: true
   belongs_to :coordinator, class_name: 'User', optional: true
@@ -18,11 +21,14 @@ class Request < ApplicationRecord
                         irrelevant: 'irrelevant' }, _suffix: true
 
   validates :is_published, inclusion: { in: [true, false] }
+  validates :subscriber_phone, phony_plausible: true
   validates_length_of :text, maximum: 160
   validates_presence_of :text, :required_volunteer_count, :subscriber, :subscriber_phone, :fulfillment_date
   validates_associated :address
 
   accepts_nested_attributes_for :address
+
+  delegate :country_code, to: :address
 
   before_validation :prepare_default
 
