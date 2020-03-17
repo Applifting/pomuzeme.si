3a875e9aa212cf999621e72d4a5287105346352c
Kamil Hanus <kamil.hanus@applifting.cz>
2020-03-17 17:06:55 +0100

    fix decorator

--- app/decorators/role_decorator.rb
+++ app/decorators/role_decorator.rb
@@ -3,11 +3,11 @@ class RoleDecorator < ApplicationDecorator
   delegate_all
   decorates :role
 
-  def actions(user)
+  def actions
     [button_delete].compact.join(' | ').html_safe
   end
 
   def button_delete
-    h.link_to 'smazat', destroy_role_admin_user_path(), method: :post, class: 'confirm', 'data-warning': 'Smazat roli?'
+    h.link_to 'smazat', '', method: :get, class: 'confirm', 'data-warning': 'Smazat roli?'
   end
 end
