2fb8cd1d1e0fceced7c7f2d2e0e6a323d40c8d3d
Kamil Hanus <kamil.hanus@applifting.cz>
2020-03-18 11:09:40 +0100

    fix point creation

--- app/services/geography/point.rb
+++ app/services/geography/point.rb
@@ -1,13 +1,13 @@
 module Geography
   class Point
     # Creates geographic point using Rgeo factory from coordinates in S-JTSK geography system
-    def self.from_coordinates(latitude:, longitude:)
-      RGeo::Geographic.spherical_factory(srid: 4326).point(latitude, longitude)
+    def self.from_coordinates(longitude:, latitude:)
+      RGeo::Geographic.spherical_factory(srid: 4326).point(longitude, latitude)
     end
 
     def self.from_s_jtsk(x:, y:)
       point = JTSK::Converter.new.to_wgs84(x, y)
-      from_coordinates latitude: point.latitude, longitude: point.longitude
+      from_coordinates longitude: point.longitude, latitude: point.latitude
     end
   end
 end
