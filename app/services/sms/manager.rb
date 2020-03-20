ba722f58033190889996ad71758323efd896426e
Tomas Landovsky <landovsky@gmail.com>
2020-03-20 21:05:08 +0100

    WIP

--- app/services/sms/manager.rb
+++ app/services/sms/manager.rb
@@ -1,14 +1,13 @@
 class Sms::Manager
-
   def send_verification_code(code, phone)
     msg = I18n.t('sms.verification', code: code)
-    #Sms::NetHost.new.send_msg(phone, msg)
+    # Sms::NetHost.new.send_msg(phone, msg)
     Sms::O2Connector.new.send_msg(phone, msg)
   end
 
-  def send_welcome_msg(phone)
-    msg = I18n.t('sms.welcome')
-    #Sms::NetHost.new.send_msg(phone, msg)
+  def send_welcome_msg(phone, group)
+    msg = group.present? ? I18n.t('sms.welcome_channel', group_name: group.name) : I18n.t('sms.welcome')
+    # Sms::NetHost.new.send_msg(phone, msg)
     Sms::O2Connector.new.send_msg(phone, msg)
   end
 end
