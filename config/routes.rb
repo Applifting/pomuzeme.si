679752ff432865dee96a8438e1197322d43622f2
Tomas Landovsky <landovsky@gmail.com>
2020-03-20 17:39:03 +0100

    Add constraint on partner slug

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     post :resend, on: :collection
   end
   root 'home#index'
-  get '/:slug', param: :slug, to: 'home#partner_signup'
+  get '/:slug', param: :slug, to: 'home#partner_signup', slug: /(?!.*?admin).*/
 
   namespace :docs do
     get '/partner-kit', to: redirect { 'https://drive.google.com/drive/folders/1w9_PVRbZ9VvE10zY0sR26f6SlmLq0xZn' }
