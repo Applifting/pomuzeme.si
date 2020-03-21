58d6b48221ce9405c5d9d2d5366d32e7606d0b9f
landovsky <435847+landovsky@users.noreply.github.com>
2020-03-21 07:22:31 +0100

    Add partner name and slug to registration confirmation sms (#135)
    * Distinguish standard welcome from partner welcome sms; spec

---
+++ spec/services/sms_service_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe Sms::Manager do
+  before(:all) { I18n.locale = :cs }
+
+  describe '.send_welcome_msg' do
+    let(:standard_text) { I18n.t 'sms.welcome' }
+    let(:partner_text)  { I18n.t 'sms.welcome_channel', group_name: 'Clovek', group_slug: 'clovek' }
+    let(:group)         { Group.new name: 'Clovek', slug: 'clovek' }
+
+    it 'sends standard welcome message to free volunteer' do
+      expect_any_instance_of(described_class).to receive(:sms_gateway).with('1234', standard_text)
+
+      described_class.send_welcome_msg '1234'
+    end
+
+    it 'sends welcome message to channel volunteer' do
+      expect_any_instance_of(described_class).to receive(:sms_gateway).with('1234', partner_text)
+
+      described_class.send_welcome_msg '1234', group
+    end
+  end
+end
