837ddb5855cbb1a7aeedabfcbed2e0dba314fb31
Kamil Hanus <ja@kamilhanus.cz>
2020-03-23 06:05:58 +0100

    load default mailer host url from ENV (#159)
    Co-authored-by: Kamil Hanus <kamil.hanus@applifting.cz>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "pomuzeme_si_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: ENV['MAILER_HOST'] }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
