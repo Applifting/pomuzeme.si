a811c619960a6de71536bcb8120bd3183c56426e
Kamil Hanus <kamil.hanus@applifting.cz>
2020-03-24 18:52:26 +0100

    dont be too strict with superadmin

--- app/models/abilities/super_admin.rb
+++ app/models/abilities/super_admin.rb
@@ -2,8 +2,6 @@ module Abilities
   module SuperAdmin
     def add_super_admin_ability(user)
       can :manage, :all
-      cannot :destroy, :all
-      can :destroy, [Volunteer, VolunteerDecorator]
     end
   end
 end
