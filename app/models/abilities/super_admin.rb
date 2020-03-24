8181809c3c6a604cca73fad8032953167e362fe5
Kamil Hanus <kamil.hanus@applifting.cz>
2020-03-24 16:11:59 +0100

    allow to destroy only volunteer to admin

--- app/models/abilities/super_admin.rb
+++ app/models/abilities/super_admin.rb
@@ -2,6 +2,8 @@ module Abilities
   module SuperAdmin
     def add_super_admin_ability(user)
       can :manage, :all
+      cannot :destroy, :all
+      can :destroy, [Volunteer, VolunteerDecorator]
     end
   end
 end
