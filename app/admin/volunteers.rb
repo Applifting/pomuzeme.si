41a2d4730d9222c5a8edc68afab1f2c5cfbd5155
Kamil Hanus <kamil.hanus@applifting.cz>
2020-03-24 13:33:42 +0100

    add confirmation dialog

--- app/admin/volunteers.rb
+++ app/admin/volunteers.rb
@@ -56,7 +56,6 @@ ActiveAdmin.register Volunteer do
     if scoped_request
       para I18n.t('active_admin.batch_actions.assign_request.title', request: scoped_request.title)
       para I18n.t('active_admin.batch_actions.assign_request.description'), class: :small
-      para
       selectable_column
     end
     id_column
