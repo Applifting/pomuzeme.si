2870e8f1a1f385e0c4126e09ea417396d713f3d3
Tomas Landovsky <landovsky@gmail.com>
2020-03-26 13:38:03 +0100

    Cleanup helper method

--- app/services/data_import_service.rb
+++ app/services/data_import_service.rb
@@ -3,9 +3,11 @@ module DataImportService
     Importer.new(filename, group).call
   end
 
-  def self.cleanup(group)
+  def self.cleanup(group, org_names)
     Volunteer.joins(:group_volunteers).where(group_volunteers: { group_id: group.id }).destroy_all
-    Request.joins(organisation: :organisation_groups).where(organisation_groups: { id: group.id }).destroy_all
+    orgs = Organisation.where(name: org_names)
+    Request.where(organisation_id: orgs.pluck(:id)).destroy_all
+    orgs.destroy_all
     Label.where(group_id: group.id).destroy_all
   end
 end
