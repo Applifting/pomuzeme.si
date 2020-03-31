b9402df4cfe602d66eaba719ae3836c3847bf13b
Kamil Hanus <kamil.hanus@applifting.cz>
2020-03-31 08:46:13 +0200

    notify assigned volunteers

--- app/services/admin/requests/volunteer_assigner.rb
+++ app/services/admin/requests/volunteer_assigner.rb
@@ -10,11 +10,22 @@ module Admin
       end
 
       def perform
+        volunteers_to_notify = []
         RequestedVolunteer.transaction do
           @volunteers.available_for(@user.organisation_group.id).each do |volunteer|
+            volunteers_to_notify << volunteer if volunteer.fcm_active?
             RequestedVolunteer.create! volunteer: volunteer, request: @request, state: @requested_state
           end
+          notify_assigned volunteers_to_notify
         end
+      end
+
+      private
+
+      def notify_assigned(volunteers)
+        return if volunteers.blank?
+
+        Push::Requests::AssignerService.new(@request.id, volunteers).perform
       end
     end
   end
