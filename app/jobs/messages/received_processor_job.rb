43d76ba27c94e5484ffcef385798ba45e24439e9
Tomas Landovsky <landovsky@gmail.com>
2020-04-03 09:39:48 +0200

    send invalid response only in notified status, mark msg read

--- app/jobs/messages/received_processor_job.rb
+++ app/jobs/messages/received_processor_job.rb
@@ -4,12 +4,14 @@ module Messages
 
     def perform(message)
       @message = message
-      return invalid_response unless valid_response?
 
       # Process response message in context of requested volunteer associations waiting for response
       RequestedVolunteer.where(volunteer_id: message.volunteer_id, state: :notified).each do |requested_volunteer|
+        return invalid_response unless valid_response?
+
         Admin::Requests::VolunteerResponseProcessor.new(requested_volunteer, response).perform
         confirm_response requested_volunteer
+        mark_message_as_read
       rescue Admin::Requests::CapacityExceededError
         capacity_exceeded_response requested_volunteer
       end
@@ -29,6 +31,10 @@ module Messages
       create_message I18n.t('sms.request.over_capacity', organisation: requested_volunteer.request.organisation.name)
     end
 
+    def mark_message_as_read
+      @message.mark_as_read
+    end
+
     def confirm_response(requested_volunteer)
       if requested_volunteer.accepted?
         create_message I18n.t('sms.request.confirmed', organisation: requested_volunteer.request.organisation.name)
@@ -46,8 +52,8 @@ module Messages
       return @response if defined? @response
 
       normalized_message = message.text.strip.downcase
-      @response = true if normalized_message.start_with? 'ano'
-      @response = false if normalized_message.start_with? 'ne'
+      @response = true if normalized_message == 'ano'
+      @response = false if normalized_message == 'ne'
       @response
     end
   end
