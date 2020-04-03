6640978ea95c2a64f19dc8055978cd64df5e14d8
Tomas Landovsky <landovsky@gmail.com>
2020-04-03 02:52:56 +0200

    test message receiving locally

--- app/jobs/messages/receiver_job.rb
+++ app/jobs/messages/receiver_job.rb
@@ -6,7 +6,7 @@ module Messages
       Rails.logger.debug 'Performing MessageReceiverJob'
       Messages::ReceiverJob.set(wait: 31.seconds).perform_later
 
-      if Rails.env.production?
+      if Rails.env.production? || ENV['RECEIVE_MESSAGES'] == 'true'
         SmsService.receive
       else
         puts 'Calling receiver service'
