09a997fefb05b1132619b0e0dbcc0b8d3b40fe53
Tomas Landovsky <landovsky@gmail.com>
2020-04-03 02:57:53 +0200

    remove sender_queue, decrease priority of receiver_queue

--- app/jobs/messages/sender_job.rb
+++ app/jobs/messages/sender_job.rb
@@ -1,7 +1,5 @@
 module Messages
   class SenderJob < ApplicationJob
-    queue_as :sender_queue
-
     def perform(message_id)
       MessagingService.send(Message.eager_load(:volunteer, :creator, request: :organisation).find(message_id))
     end
