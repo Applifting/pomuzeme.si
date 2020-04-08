ca8083ab614c412c7307d4eb520b5b39dc5e6942
Vaclav Pavlicek <venda.pavlicek@gmail.com>
2020-04-08 15:40:49 +0200

    Landing page menu (#195)
    * add desktop menu layout
    
    * add mobile menu
    
    * refactor scroll on "Organizuji dobrovolníky" button
    
    * add hiding menu on scroll down
    
    * refactor code
    
    * move menu to its stylesheet
    
    * linked menus to proper sections, made logo clickable
    
    Co-authored-by: Tomas Landovsky <landovsky@gmail.com>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels");
 import 'materialize-css/dist/js/materialize'
 require("src/materialize");
 require("src/google_autocomplete");
+require("src/menu");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
