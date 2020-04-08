4ff0cae04377071c72105c2621b938e97e7f0285
Kamil Hanus <kamil.hanus@applifting.cz>
2020-04-08 12:42:20 +0200

    do not accumulate receiver jobs

--- app/jobs/messages/receiver_job.rb
+++ app/jobs/messages/receiver_job.rb
@@ -7,7 +7,7 @@ module Messages
     rescue StandardError => e
       Raven.capture_exception e
     ensure
-      Messages::ReceiverJob.perform_later
+      Messages::ReceiverJob.perform_later unless already_performing? || already_enqueued?
     end
 
     def perform
@@ -17,7 +17,21 @@ module Messages
         SmsService.receive
       else
         puts 'Calling receiver service'
+        sleep 10
       end
+    end
+
+    private
+
+    def already_performing?
+      Sidekiq::Workers.new.to_a.any? do |worker|
+        data = worker.try :[], 2 # data are third element of worker array
+        (data&.dig('payload', 'queue') == 'receiver_queue') && (data&.dig('payload', 'jid') != provider_job_id)
+      end
+    end
+
+    def already_enqueued?
+      Sidekiq::ScheduledSet.new.scan("Messages::ReceiverJob").any?
     end
   end
 end
