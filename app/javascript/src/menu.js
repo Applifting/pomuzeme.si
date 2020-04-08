66a5422012a495f02b1dec6ee0624a0b708cb0ba
Tomas Landovsky <landovsky@gmail.com>
2020-04-08 15:39:05 +0200

    linked menus to proper sections, made logo clickable

--- app/javascript/src/menu.js
+++ app/javascript/src/menu.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     const menuButton = document.querySelector('#js-menu-button');
     const closeMenuButton = document.querySelector('#js-close-menu-button');
-    const mobileLinks = document.querySelectorAll('#js-mobile-menu-list .slide-link');
-    const desktopLinks = document.querySelectorAll('#js-desktop-menu-list .slide-link');
+    const mobileLinks = document.querySelectorAll('.mobile-menu .slide-link');
+    const desktopLinks = document.querySelectorAll('.main-header .slide-link');
     const mobileMenu = document.querySelector('#js-mobile-menu-container');
     const body = document.querySelector('body');
     const wannaJoinButton = document.querySelector('#js-mobile-menu-container .main-button');
@@ -15,11 +15,11 @@ $(document).ready(function () {
     };
 
     const scrollToId = id => document.querySelector('#' + id).scrollIntoView();
-    const smoothlyScrollToId = id => document.querySelector('#' + id).scrollIntoView({behavior: 'smooth'});
+    const smoothlyScrollToId = id => document.querySelector('#' + id).scrollIntoView({ behavior: 'smooth' });
 
     menuButton.onclick = () => mobileMenu.classList.add('open');
 
-    const linkIds = ['intro', 'guide', 'counts', 'organizace'];
+    const linkIds = ['home', 'intro', 'guide', 'news', 'partners'];
 
     mobileLinks.forEach((link, index) => link.addEventListener('click', () => {
         closeCallback();
