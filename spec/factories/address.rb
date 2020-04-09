de37dce734db1fc5e475b37815a95a370c77de98
Kamil Hanus <kamil.hanus@applifting.cz>
2020-04-09 16:27:34 +0200

    wip volunteer spec

--- spec/factories/address.rb
+++ spec/factories/address.rb
@@ -7,6 +7,7 @@ FactoryBot.define do
     city { FFaker::AddressDE.city }
     city_part { FFaker::AddressDE.city }
     postal_code { FFaker::AddressUK.postcode }
+    coordinate { Geography::Point.from_coordinates longitude: 14.4548664, latitude: 50.0941811 } # Applifting HQ
     geo_entry_id { 43 }
     geo_unit_id { 43 }
     geo_provider { 'google_places' }
