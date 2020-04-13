d94485b61b5f15ba6e8e5f1e894e5173dce49734
Kamil Hanus <kamil.hanus@applifting.cz>
2020-04-13 14:20:54 +0200

    add flutter payload

--- app/services/push/sender_service.rb
+++ app/services/push/sender_service.rb
@@ -10,12 +10,18 @@ module Push
       Rpush::Gcm::Notification.new.tap do |n|
         n.app = Rpush::Gcm::App.find_by_name(ENV['PUSH_APP_NAME'])
         n.registration_ids = @receivers
-        n.data = @payload_data
+        n.data = @payload_data.merge flutter_payload
         n.priority = 'normal'
         n.content_available = true
         n.notification = @notification_data
         n.save!
       end
+    end
+
+    private
+
+    def flutter_payload
+      { click_action: 'FLUTTER_NOTIFICATION_CLICK' }.freeze
     end
   end
 end
