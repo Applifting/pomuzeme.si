a3b35be7edf3c46b0befecf89ddaad21568338e4
Kamil Hanus <kamil.hanus@applifting.cz>
2020-04-15 10:20:30 +0200

    remove debug logs

--- config/initializers/message_receiver.rb
+++ config/initializers/message_receiver.rb
@@ -1,14 +1,7 @@
-Rails.logger.debug 'initializer started'
-# return unless defined?(Rails::Server)
-Rails.logger.debug 'rails server defined'
-
 require 'sidekiq/api'
 
 Sidekiq.configure_client do |_config|
-  Rails.logger.debug 'configuring client'
   Rails.application.config.after_initialize do
-    Rails.logger.debug 'application initialized'
-
     Messages::ReceiverJob.perform_later
   end
 end
