d423a48e0f314c0975e157294478b2ded5ccb37c
Kamil Hanus <kamil.hanus@applifting.cz>
2020-04-15 10:04:39 +0200

    modify receiver job

--- config/initializers/message_receiver.rb
+++ config/initializers/message_receiver.rb
@@ -1,9 +1,14 @@
+Rails.logger.debug 'initializer started'
 return unless defined?(Rails::Server)
+Rails.logger.debug 'rails server defined'
 
 require 'sidekiq/api'
 
 Sidekiq.configure_client do |_config|
+  Rails.logger.debug 'configuring client'
   Rails.application.config.after_initialize do
-    Messages::ReceiverJob.perform_later if ::Sidekiq::ScheduledSet.new.scan("Messages::ReceiverJob").none?
+    Rails.logger.debug 'application initialized'
+
+    Messages::ReceiverJob.perform_later
   end
 end
