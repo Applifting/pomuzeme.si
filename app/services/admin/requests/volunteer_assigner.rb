1fe31d864c310cb3670d5112ba6d4d80f61eec65
Kamil Hanus <kamil.hanus@applifting.cz>
2020-04-20 12:25:47 +0200

    volunteer assigner spec

--- app/services/admin/requests/volunteer_assigner.rb
+++ app/services/admin/requests/volunteer_assigner.rb
@@ -1,11 +1,14 @@
 module Admin
   module Requests
     class VolunteerAssigner
+
+      DEFAULT_STATE = 'to_be_notified'.freeze
+
       def initialize(user, request, volunteers, requested_state: nil)
         @user = user
         @request = request
         @volunteers = volunteers
-        @requested_state = requested_state || :to_be_notified
+        @requested_state = requested_state || DEFAULT_STATE
       end
 
       def perform
