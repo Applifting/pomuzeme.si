ed3ee8341ea0473023edf7cf53ea371881019d8b
Kamil Hanus <kamil.hanus@applifting.cz>
2020-04-21 16:49:51 +0200

    messaging service spec

--- spec/services/messaging_service_spec.rb
+++ spec/services/messaging_service_spec.rb
@@ -20,11 +20,45 @@ describe MessagingService do
     end
   end
 
+  describe '.send_message' do
+    context 'notification via SMS' do
+      before { message.sms! }
+
+      it 'sends message by SmsService' do
+        expect(SmsService).to receive(:send_message)
+        MessagingService.send_message message
+      end
+
+      it 'calls #message_sent callback' do
+        mock_response = OpenStruct.new
+        allow(SmsService).to receive(:send_message).and_yield(mock_response)
+        expect(MessagingService::Callbacks).to receive(:message_sent).with(instance_of(MessagingService::OutgoingMessage), mock_response)
+        MessagingService.send_message message
+      end
+    end
+
+    context 'notification via Push' do
+      before { message.push! }
+
+      it 'sends message by PushService' do
+        expect(PushService).to receive(:send_message)
+        MessagingService.send_message message
+      end
+
+      it 'calls #message_sent callback' do
+        mock_response = OpenStruct.new
+        allow(PushService).to receive(:send_message).and_yield(mock_response)
+        expect(MessagingService::Callbacks).to receive(:message_sent).with(instance_of(MessagingService::OutgoingMessage), mock_response)
+        MessagingService.send_message message
+      end
+    end
+  end
+
   describe '.callback' do
-    it 'delegates callback calling to common module' do
-      expect(MessagingService::Callbacks).to receive(:message_sent)
-                                                 .with([])
-      MessagingService.callback :message_sent, []
+    it 'delegates callback call to common module' do
+      mock_payload = OpenStruct.new
+      expect(MessagingService::Callbacks).to receive(:delivery_report_received).with(mock_payload)
+      MessagingService.callback :delivery_report_received, mock_payload
     end
   end
 end
