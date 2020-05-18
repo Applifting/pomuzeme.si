952be2031c11e1c3cc12d453ed84b5429adcfa34
Tomas Landovsky <landovsky@gmail.com>
2020-05-18 17:12:55 +0200

    UK landing page updates

---
+++ app/helpers/organisation_helper.rb
@@ -0,0 +1,10 @@
+module OrganisationHelper
+  def logo_with_link(organisation_symbol)
+    logo = image_tag asset_path("logo-#{organisation_symbol}.svg"), alt: translate(:alt, organisation_symbol), title: translate(:title, organisation_symbol)
+    link_to logo, translate(:url, organisation_symbol), target: '_blank'
+  end
+
+  def translate(symbol, organisation_symbol)
+    I18n.t "landingpage.orgs.members.#{organisation_symbol}.#{symbol}"
+  end
+end
