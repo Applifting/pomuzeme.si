188c5413df98b47886569f42b5bb3d3a97791180
Kamil Hanus <ja@kamilhanus.cz>
2020-05-18 21:19:52 +0200

    notify updated volunteers (#268)
    * notify updated volunteers
    
    * clear controller action
    
    Co-authored-by: Kamil Hanus <kamil.hanus@applifting.cz>

--- app/admin/requested_volunteers.rb
+++ app/admin/requested_volunteers.rb
@@ -15,7 +15,10 @@ ActiveAdmin.register RequestedVolunteer do
 
     def update
       super do |success, failure|
-        success.html { redirect_to admin_organisation_request_path(resource.request_id) }
+        success.html do
+          notify_volunteers_updated if should_notify_update?
+          redirect_to admin_organisation_request_path(resource.request_id)
+        end
         failure.html { render :new }
       end
     end
@@ -25,6 +28,17 @@ ActiveAdmin.register RequestedVolunteer do
         success.html { redirect_to admin_organisation_request_path(resource.request_id) }
         failure.html { render :new }
       end
+    end
+
+    private
+
+    def notify_volunteers_updated
+      Admin::Requests::VolunteerNotifier.new(current_user, resource.request).notify_updated
+    end
+
+    # send update only in case volunteer is has accepted request and sensitive information is visible
+    def should_notify_update?
+      resource.accepted? && resource.visible_sensitive?
     end
   end
 end
