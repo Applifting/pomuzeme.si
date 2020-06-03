229f64dccc2afa040b7ba82dd077c054aaa53fd9
Kamil Hanus <kamil.hanus@applifting.cz>
2020-06-03 07:21:27 +0200

    refactor

--- spec/services/api/volunteer/register_spec.rb
+++ spec/services/api/volunteer/register_spec.rb
@@ -19,7 +19,7 @@ describe Api::Volunteer::Register do
       expect { Api::Volunteer::Register.new(volunteer, volunteer_data).perform }
         .to change { volunteer.reload.first_name }.from('').to('Test')
         .and change { volunteer.reload.last_name }.from('').to('Testic')
-                                                  .and change { volunteer.addresses.count }.from(0).to(1)
+        .and change { volunteer.addresses.count }.from(0).to(1)
       expect(volunteer.addresses.first.geo_entry_id).to eq geocoder_search_mock.first.data['place_id']
     end
 
--- spec/support/messaging_service_helper.rb
+++ spec/support/messaging_service_helper.rb
@@ -1,6 +1,6 @@
 module MessagingServiceHelper
 
-  def message_response(message_id: nil, number: nil, text: nil, timestamp: nil)
+  def mock_message_response(message_id: nil, number: nil, text: nil, timestamp: nil)
     OpenStruct.new(channel_msg_id: message_id || 42,
                    from_number: number || '123456789',
                    text: text || 'response text',
