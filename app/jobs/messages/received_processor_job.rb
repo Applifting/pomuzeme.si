b2ea9154843ef57a08fef5fb651614bc64ee8cce
Tomas Landovsky <landovsky@gmail.com>
2020-09-01 21:22:59 +0200

    Fixed incorrect response to offer acceptance

--- app/jobs/messages/received_processor_job.rb
+++ app/jobs/messages/received_processor_job.rb
@@ -1,19 +1,21 @@
 module Messages
   class ReceivedProcessorJob < ApplicationJob
-    attr_reader :message
+    attr_reader :message, :requested_volunteer, :request
 
     def perform(message)
       @message = message
 
       # Process response message in context of requested volunteer associations waiting for response
       RequestedVolunteer.eager_load(:request, :volunteer).where(volunteer_id: message.volunteer_id, state: :notified).each do |requested_volunteer|
+        @requested_volunteer = requested_volunteer
+        @request             = requested_volunteer.request
         return invalid_response unless valid_response?
 
-        Common::Request::ResponseProcessor.new(requested_volunteer.request, requested_volunteer.volunteer, response).perform
-        confirm_response requested_volunteer
+        Common::Request::ResponseProcessor.new(request, requested_volunteer.volunteer, response).perform
+        confirm_response
         mark_message_as_read
       rescue Common::Request::CapacityExceededError
-        capacity_exceeded_response requested_volunteer
+        capacity_exceeded_response
       end
     end
 
@@ -24,25 +26,25 @@ module Messages
     end
 
     def invalid_response
-      volunteer = Volunteer.find(@message.volunteer_id)
+      volunteer = Volunteer.find(message.volunteer_id)
       text      = I18n.t('sms.request.unrecognized')
 
       SmsService.send_text volunteer.phone, text
     end
 
-    def capacity_exceeded_response(requested_volunteer)
-      create_message I18n.t('sms.request.over_capacity', organisation: requested_volunteer.request.organisation.name)
+    def capacity_exceeded_response
+      create_message I18n.t('sms.request.over_capacity', organisation: request.organisation.name)
     end
 
     def mark_message_as_read
       @message.mark_as_read
     end
 
-    def confirm_response(requested_volunteer)
-      msg_type = requested_volunteer.accepted? ? 'sms.request.confirmed' : 'sms.request.rejected'
+    def confirm_response
+      msg_type = response ? 'sms.request.confirmed' : 'sms.request.rejected'
       create_message I18n.t(msg_type,
-                            identifier: requested_volunteer.request.identifier,
-                            organisation: requested_volunteer.request.organisation.name)
+                            identifier: request.identifier,
+                            organisation: request.organisation.name)
     end
 
     def create_message(text)
@@ -50,7 +52,8 @@ module Messages
                                       message_type: :other,
                                       channel: :sms,
                                       text: text,
-                                      volunteer_id: message.volunteer_id
+                                      volunteer_id: message.volunteer_id,
+                                      request: request
     end
 
     def response
