a829a204a2475be7721abfb7c89163c2324188b4
Tomas Landovsky <landovsky@gmail.com>
2020-10-29 22:30:29 +0100

    Scroll to top dirty fix

--- app/javascript/src/hard_reload.js
+++ app/javascript/src/hard_reload.js
@@ -1,8 +1,12 @@
 $(document).on('turbolinks:load', function () {
+  $("html, body").animate({
+    scrollTop: 0
+      }, "slow");
   $('a.hard_reload').click(function(event) {
     let goTo = $(this).attr('data-target')
-    console.log('going to:', goTo)
     location.replace(goTo || '/#')
-    location.reload
   })
+})
+
+$(document).on('load', function() {
 })
