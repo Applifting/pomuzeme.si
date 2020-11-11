2c20748667d57ad6fe4bf91e68d591743c9a2ff5
Tomas Landovsky <landovsky@gmail.com>
2020-11-11 12:09:30 +0100

    Fix recaptcha result

--- app/services/recaptchable.rb
+++ app/services/recaptchable.rb
@@ -3,11 +3,18 @@ module Recaptchable
     score_threshold = threshold&.to_f
 
     if score_threshold.present?
-      recaptcha = verify_recaptcha(action: action, minimum_score: score_threshold)
-      model.errors[:recaptcha] << 'si myslí, že jste robot. Zkusíte to znovu?' unless recaptcha
-      Rails.logger.warn "Recaptcha failed: #{recaptcha_reply}"
-      Raven.capture_exception AuthorisationError.new(:recaptcha, model) unless recaptcha
-      recaptcha
+      verify_recaptcha(action: action, minimum_score: score_threshold)
+      recaptcha_response = recaptcha_reply
+      recaptcha_result   = recaptcha_response['success'] == true
+
+      if !recaptcha_result
+        model.errors[:recaptcha] << 'si myslí, že jste robot. Zkusíte to znovu?'
+        Rails.logger.warn "Recaptcha failed: #{recaptcha_response}"
+        Raven.capture_exception AuthorisationError.new(:recaptcha, model)
+        puts "RECAPTCHA RESULT: #{recaptcha_result}" if Rails.env.development?
+      end
+
+      recaptcha_result
     else
       true
     end
