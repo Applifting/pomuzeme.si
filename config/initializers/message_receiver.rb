4509c45e50e10d255844568e5bbf3657d10e142e
Tomas Landovsky <landovsky@gmail.com>
2022-04-21 13:33:31 +0200

    Disable receiving messages

--- config/initializers/message_receiver.rb
+++ config/initializers/message_receiver.rb
@@ -1,15 +1,15 @@
-Rails.logger.debug Rails.env
-return if Rails.env.test?
+# Rails.logger.debug Rails.env
+# return if Rails.env.test?
 
-require 'sidekiq/api'
+# require 'sidekiq/api'
 
-Sidekiq.configure_client do |_config|
-  Rails.application.config.after_initialize do
-    Messages::ReceiverJob.perform_later unless ENV['DISABLE_SMS_RECEIVER'] == 'true'
-  end
-end
+# Sidekiq.configure_client do |_config|
+#   Rails.application.config.after_initialize do
+#     Messages::ReceiverJob.perform_later unless ENV['DISABLE_SMS_RECEIVER'] == 'true'
+#   end
+# end
 
-if Rails.env.development? && ENV['INLINE_SIDEKIQ'] == 'true'
-  require 'sidekiq/testing'
-  Sidekiq::Testing.inline!
-end
+# if Rails.env.development? && ENV['INLINE_SIDEKIQ'] == 'true'
+#   require 'sidekiq/testing'
+#   Sidekiq::Testing.inline!
+# end
